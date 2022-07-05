2cfcf670eb4d66d8ffabf9a91372eee934ca9531
Lokesh Jain <luckycreationsindia@gmail.com>
2022-07-06 01:32:03 +0530

    *Add Automated Testing

---
+++ test/task_manager.js
@@ -0,0 +1,36 @@
+let expect = require("chai").expect;
+require('../app');
+let ObjectId = require("mongoose").mongo.ObjectId
+let CategoryService = require("../services/category");
+
+describe("***Task Manager - Automated Test***", function () {
+    describe("Testing Category Service", function () {
+        let Category = {"name": "Test Category", "description": "Test Description"}
+        it("Add Category", function (done) {
+            CategoryService.add(Category).then((result) => {
+                Category['_id'] = result['_id']
+                expect(result).to.have.property('_id')
+                done()
+            }).catch(done);
+        });
+        it("Update Category", function (done) {
+            CategoryService.update(ObjectId(Category['_id']), {"description": "Test Description Updated"}).then((result) => {
+                expect(result.description).equal('Test Description Updated');
+                done()
+            }).catch(done);
+        });
+        it("Load Categories", function (done) {
+            CategoryService.load().then((result) => {
+                expect(result).to.have.length.gt(0);
+                expect(result[0]).to.have.property('_id');
+                done()
+            }).catch(done);
+        });
+        it("Delete Category", function (done) {
+            CategoryService.remove(ObjectId(Category['_id'])).then((result) => {
+                expect(result).equal(true);
+                done()
+            }).catch(done);
+        });
+    });
+});
