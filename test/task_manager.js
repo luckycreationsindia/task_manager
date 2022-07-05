81bebd8a6c94aa4cd98ca5e8ff69a08aac38c0cf
Lokesh Jain <luckycreationsindia@gmail.com>
2022-07-06 01:41:20 +0530

    *Few changes to tests

--- test/task_manager.js
+++ test/task_manager.js
@@ -3,33 +3,42 @@ require('../app');
 let ObjectId = require("mongoose").mongo.ObjectId
 let CategoryService = require("../services/category");
 
-describe("***Task Manager - Automated Test***", function () {
+describe("Task Manager - Automated Test", function () {
     describe("Testing Category Service", function () {
         let Category = {"name": "Test Category", "description": "Test Description"}
         it("Add Category", function (done) {
             CategoryService.add(Category).then((result) => {
-                Category['_id'] = result['_id']
-                expect(result).to.have.property('_id')
-                done()
+                setTimeout(function () {
+                    Category['_id'] = result['_id']
+                    expect(result).to.have.property('_id')
+                    done()
+                }, 2000)
+
             }).catch(done);
         });
         it("Update Category", function (done) {
             CategoryService.update(ObjectId(Category['_id']), {"description": "Test Description Updated"}).then((result) => {
                 expect(result.description).equal('Test Description Updated');
-                done()
+                setTimeout(function () {
+                    done()
+                }, 1000);
             }).catch(done);
         });
         it("Load Categories", function (done) {
             CategoryService.load().then((result) => {
-                expect(result).to.have.length.gt(0);
-                expect(result[0]).to.have.property('_id');
-                done()
+                setTimeout(function () {
+                    expect(result).to.have.length.gt(0);
+                    expect(result[0]).to.have.property('_id');
+                    done()
+                }, 1000);
             }).catch(done);
         });
         it("Delete Category", function (done) {
             CategoryService.remove(ObjectId(Category['_id'])).then((result) => {
-                expect(result).equal(true);
-                done()
+                setTimeout(function () {
+                    expect(result).equal(true);
+                    done()
+                }, 1000);
             }).catch(done);
         });
     });
